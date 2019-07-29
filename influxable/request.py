8584425be000d0ee4742b77c74feffe5d4fb159b
Javid Mougamadou <javidjms0@gmail.com>
2019-07-29 23:24:03 +0200

    [feat]: Add CRUD method to InfluxDBRequest

--- influxable/request.py
+++ influxable/request.py
@@ -9,3 +9,21 @@ class InfluxDBRequest(requests.Session):
 
     def request(self, method, url, **kwargs):
         return super().request(method, urljoin(self.base_url, url), **kwargs)
+
+    def head(self, url, **kwargs):
+        return super().head(urljoin(self.base_url, url), **kwargs)
+
+    def get(self, url, **kwargs):
+        return super().get(urljoin(self.base_url, url), **kwargs)
+
+    def post(self, url, **kwargs):
+        return super().post(urljoin(self.base_url, url), **kwargs)
+
+    def put(self, url, **kwargs):
+        return super().put(urljoin(self.base_url, url), **kwargs)
+
+    def patch(self, url, **kwargs):
+        return super().patch(urljoin(self.base_url, url), **kwargs)
+
+    def delete(self, url, **kwargs):
+        return super().delete(urljoin(self.base_url, url), **kwargs)
