c051eef66d10f9c42a820ae9b18e885f8a51a153
Javid Mougamadou <javidjms0@gmail.com>
2019-07-29 23:23:27 +0200

    [feat]: Set inheritance to InfluxDBRequest based on requests Session

--- influxable/request.py
+++ influxable/request.py
@@ -1,4 +1,11 @@
-class InfluxDBRequest:
+import requests
+from urllib.parse import urljoin
+
+
+class InfluxDBRequest(requests.Session):
     def __init__(self, base_url):
         super().__init__()
         self.base_url = base_url
+
+    def request(self, method, url, **kwargs):
+        return super().request(method, urljoin(self.base_url, url), **kwargs)
