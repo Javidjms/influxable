6da0b3c28b5e4449a17f55916a1c33909d6a7bcf
Javid Mougamadou <javidjms0@gmail.com>
2019-07-29 23:29:25 +0200

    [feat]: Add get_debug_vars staticmethod in InfluxDBApi

--- influxable/api.py
+++ influxable/api.py
@@ -8,3 +8,9 @@ class InfluxDBApi:
         params = {'seconds': seconds}
         res = request.get(url=url, params=json.dumps(params))
         return res.json()
+
+    @staticmethod
+    def get_debug_vars(request):
+        url = '/debug/vars'
+        res = request.get(url=url)
+        return res.json()
