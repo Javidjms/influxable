8f7161f93660ec46f8ab8ac32932016ef1f85593
Javid Mougamadou <javidjms0@gmail.com>
2019-07-29 23:17:36 +0200

    [feat]: Add INFLUXDB_PASSWORD env in settings.py

--- influxable/settings.py
+++ influxable/settings.py
@@ -2,3 +2,4 @@ import os
 
 INFLUXDB_URL = os.getenv('INFLUXDB_URL', 'http://localhost:8086')
 INFLUXDB_USER = os.getenv('INFLUXDB_USER', 'admin')
+INFLUXDB_PASSWORD = os.getenv('INFLUXDB_PASSWORD', 'changeme')
