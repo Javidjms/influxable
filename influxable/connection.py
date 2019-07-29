4d7b26f4657393a2e3368da3ff0b0710f14b92a4
Javid Mougamadou <javidjms0@gmail.com>
2019-07-29 23:25:11 +0200

    [feat]: Add __init__ for Connection

--- influxable/connection.py
+++ influxable/connection.py
@@ -1,2 +1,13 @@
+from . import settings
+from .request import InfluxDBRequest
+
+
 class Connection:
-    pass
+    def __init__(self):
+        self.base_url = settings.INFLUXDB_URL
+        self.user = settings.INFLUXDB_USER
+        self.password = settings.INFLUXDB_PASSWORD
+        self.database_name = settings.INFLUXDB_DATABASE_NAME
+
+        self.request = InfluxDBRequest(self.base_url)
+        self.request.auth = (self.user, self.password)
