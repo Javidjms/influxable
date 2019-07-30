106004e7489a0acaef6be33cd6be644e604103a8
Javid Mougamadou <javidjms0@gmail.com>
2019-07-31 00:06:48 +0200

    [feat]: Integrate ping, execute_query, write_points in Influxable class

--- influxable/app.py
+++ influxable/app.py
@@ -1,9 +1,21 @@
-from .helpers.decorators import Singleton
+from .api import InfluxDBApi
 from .connection import Connection
+from .helpers.decorators import Singleton
 
 
 @Singleton
 class Influxable:
     def __init__(self):
         self.connection = Connection()
-    
+
+    def ping(self, *args, **kwargs):
+        request = self.connection.request
+        return InfluxDBApi.ping(request, *args, **kwargs)
+
+    def execute_query(self, *args, **kwargs):
+        request = self.connection.request
+        return InfluxDBApi.execute_query(request, *args, **kwargs)
+
+    def write_points(self, *args, **kwargs):
+        request = self.connection.request
+        return InfluxDBApi.write_points(request, *args, **kwargs)
