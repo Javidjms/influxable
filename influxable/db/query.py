7f163d599e32191c968d595de337867a0b67f097
Javid Mougamadou <javidjms0@gmail.com>
2019-08-29 22:03:49 +0200

    [feat]: Add execute method in RawQuery

--- influxable/db/query.py
+++ influxable/db/query.py
@@ -2,4 +2,9 @@ class RawQuery:
     def __init__(self, str_query):
         self.str_query = str_query
 
-    
+    def execute(self):
+        return self.raw_response
+
+    @property
+    def raw_response(self):
+        return self._resolve()
