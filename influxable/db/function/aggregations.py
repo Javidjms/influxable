326dc34ef72834b46ef3430900398f6fb9ae9fda
Javid Mougamadou <javidjms0@gmail.com>
2019-09-08 13:13:58 +0200

    [feat]: Add MEAN aggregation operator

--- influxable/db/function/aggregations.py
+++ influxable/db/function/aggregations.py
@@ -3,3 +3,4 @@ from . import _generate_function
 Count = _generate_function('COUNT')
 Distinct = _generate_function('DISTINCT')
 Integral = _generate_function('INTEGRAL')
+Mean = _generate_function('MEAN')
