926eb6966643c37f4f7f23713c5296c136f25115
Javid Mougamadou <javidjms0@gmail.com>
2019-09-08 13:16:30 +0200

    [feat]: Add LAST selector operator

--- influxable/db/function/selectors.py
+++ influxable/db/function/selectors.py
@@ -2,3 +2,4 @@ from . import _generate_function, _generate_function_with_param
 
 Bottom = _generate_function_with_param('BOTTOM')
 First = _generate_function('FIRST')
+Last = _generate_function('LAST')
