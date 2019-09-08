55cb72741b9162289fd1a0b326f897e588798e71
Javid Mougamadou <javidjms0@gmail.com>
2019-09-08 13:12:12 +0200

    [feat]: Add _generate_function_with_param function

--- influxable/db/function/__init__.py
+++ influxable/db/function/__init__.py
@@ -10,3 +10,15 @@ def _generate_function(identifier):
                 field = self.field
             return '{}({})'.format(identifier, field)
     return Function
+
+
+def _generate_function_with_param(identifier):
+    class Function:
+        def __init__(self, n, *fields):
+            self.n = n
+            self.fields = fields
+
+        def evaluate(self):
+            fields = ', '.join(self.fields)
+            return '{}({}, {})'.format(identifier, fields, self.n)
+    return Function
