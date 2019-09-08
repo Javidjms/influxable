f06000aed1a8f7a47865a0ce2b1d0d880aeca970
Javid Mougamadou <javidjms0@gmail.com>
2019-09-08 13:16:35 +0200

    [feat]: Add MAX selector operator

--- influxable/db/function/selectors.py
+++ influxable/db/function/selectors.py
@@ -3,3 +3,4 @@ from . import _generate_function, _generate_function_with_param
 Bottom = _generate_function_with_param('BOTTOM')
 First = _generate_function('FIRST')
 Last = _generate_function('LAST')
+Max = _generate_function('MAX')
