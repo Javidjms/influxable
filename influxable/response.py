491d8be707784b199757e3f1f8c3cbefd9e80be1
Javid Mougamadou <javidjms0@gmail.com>
2019-09-10 11:31:52 +0200

    [feat]: Add series property for InfluxDBResponse

--- influxable/response.py
+++ influxable/response.py
@@ -6,6 +6,17 @@ class InfluxDBResponse:
     def raw(self):
         return self._raw_json
 
+    @property
+    def series(self):
+        if 'results' in self.raw:
+            results = self.raw['results']
+            if len(results):
+                result = results[0]
+                if 'series' in result:
+                    return [InfluxDBResponseSerie(s) for s in result['series']]
+        return None
+
+
 class InfluxDBResponseSerie:
     def __init__(self, json_serie):
         self._raw_json_serie = json_serie
