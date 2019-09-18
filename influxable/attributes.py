4f0701859d321de735cae3dd676e6122bd848acf
Javid Mougamadou <javidjms0@gmail.com>
2019-09-18 22:28:47 +0200

    [feat]: Add TIMESTAMP_CONVERT_RATIO constant

--- influxable/attributes.py
+++ influxable/attributes.py
@@ -1,3 +1,6 @@
+from .helpers.utils import inv
+
+
 class TimestampPrecision:
     HOURS = 'h'
     MICROSECONDS = 'u'
@@ -5,3 +8,13 @@ class TimestampPrecision:
     MINUTES = 'm'
     NANOSECONDS = 'ns'
     SECONDS = 's'
+
+
+TIMESTAMP_CONVERT_RATIO = {
+    TimestampPrecision.HOURS: inv(60 * 60),
+    TimestampPrecision.MICROSECONDS: 1 * 1000 * 1000,
+    TimestampPrecision.MILLISECONDS: 1000,
+    TimestampPrecision.MINUTES: inv(60),
+    TimestampPrecision.NANOSECONDS: 1 * 1000 * 1000 * 1000,
+    TimestampPrecision.SECONDS: 1,
+}
