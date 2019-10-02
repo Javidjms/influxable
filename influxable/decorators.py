a6fa66a543b135daaebd86369bfacde54013a701
Javid Mougamadou <javidjms0@gmail.com>
2019-10-02 15:13:09 +0200

    [feat]: Handle ConnectionError in raise_if_error decorator

--- influxable/decorators.py
+++ influxable/decorators.py
@@ -1,5 +1,13 @@
+import requests
+from . import exceptions
+
+
 def raise_if_error(func):
     def func_wrapper(*args, **kwargs):
-        res = func(*args, **kwargs)
+        try:
+            res = func(*args, **kwargs)
+            res.raise_for_status()
+        except requests.exceptions.ConnectionError as err:
+            raise exceptions.InfluxDBConnectionError(err)
         return res
     return func_wrapper
