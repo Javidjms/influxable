12a80c7e35f92c3771725c19921006ed497150bf
Javid Mougamadou <javidjms0@gmail.com>
2019-10-02 15:18:04 +0200

    [feat]: Add InfluxDBBadRequestError class

--- influxable/exceptions.py
+++ influxable/exceptions.py
@@ -5,3 +5,12 @@ class InfluxDBException(Exception):
 class InfluxDBConnectionError(InfluxDBException):
     pass
 
+
+class InfluxDBBadRequestError(InfluxDBException):
+    MESSAGE_PLACEHOLDER = 'Bad request for params : {params}'
+
+    def __init__(self, params):
+        self.message = self.MESSAGE_PLACEHOLDER.format(params=params)
+        super().__init__(self.message)
+
+
