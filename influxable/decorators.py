3ebbc29ff51e4929c227d859831889e4261d79ba
Javid Mougamadou <javidjms0@gmail.com>
2019-10-02 15:14:30 +0200

    [feat]: Handme InfluxDBConnectionError in raise_if_error decorator

--- influxable/decorators.py
+++ influxable/decorators.py
@@ -5,11 +5,14 @@ from . import exceptions
 def raise_if_error(func):
     def func_wrapper(*args, **kwargs):
         try:
+            params = kwargs.get('params', {})
             res = func(*args, **kwargs)
             res.raise_for_status()
         except requests.exceptions.ConnectionError as err:
             raise exceptions.InfluxDBConnectionError(err)
         except requests.exceptions.HTTPError as err:
+            if res.status_code == 400:
+                raise exceptions.InfluxDBBadRequestError(params)
             raise err
         return res
     return func_wrapper
