0f990e7c350d875167f3896deab6d13017486d90
Javid Mougamadou <javidjms0@gmail.com>
2019-10-02 15:15:45 +0200

    [feat]: Integrate raise_if_error decorator in InfluxDBRequest

--- influxable/request.py
+++ influxable/request.py
@@ -1,5 +1,6 @@
 import requests
 from urllib.parse import urljoin
+from .decorators import raise_if_error
 
 
 class InfluxDBRequest(requests.Session):
@@ -9,30 +10,37 @@ class InfluxDBRequest(requests.Session):
         self.database_name = database_name
         self.auth = auth
 
+    @raise_if_error
     def request(self, method, url, **kwargs):
         full_url = urljoin(self.base_url, url)
         return super().request(method, url=full_url, **kwargs)
 
+    @raise_if_error
     def head(self, url, **kwargs):
         full_url = urljoin(self.base_url, url)
         return super().head(full_url, **kwargs)
 
+    @raise_if_error
     def get(self, url, **kwargs):
         full_url = urljoin(self.base_url, url)
         return super().get(full_url, **kwargs)
 
+    @raise_if_error
     def post(self, url, **kwargs):
         full_url = urljoin(self.base_url, url)
         return super().post(full_url, **kwargs)
 
+    @raise_if_error
     def put(self, url, **kwargs):
         full_url = urljoin(self.base_url, url)
         return super().put(full_url, **kwargs)
 
+    @raise_if_error
     def patch(self, url, **kwargs):
         full_url = urljoin(self.base_url, url)
         return super().patch(full_url, **kwargs)
 
+    @raise_if_error
     def delete(self, url, **kwargs):
         full_url = urljoin(self.base_url, url)
         return super().delete(full_url, **kwargs)
