dfdcbc90807047898ff7b30270769b8d005fb98e
Javid Mougamadou <javidjms0@gmail.com>
2019-10-02 22:30:10 +0200

    [feat]: Handle InfluxDBBadQueryError in raise_if_error decorator

--- influxable/decorators.py
+++ influxable/decorators.py
@@ -1,3 +1,4 @@
+import json
 import requests
 from . import exceptions
 
@@ -5,12 +6,23 @@ from . import exceptions
 def raise_if_error(func):
     def func_wrapper(*args, **kwargs):
         try:
+            request = args[0]
             params = kwargs.get('params', {})
             res = func(*args, **kwargs)
+            try:
+                json_res = res.json()
+            except json.decoder.JSONDecodeError:
+                json_res = {}
             res.raise_for_status()
         except requests.exceptions.ConnectionError as err:
             raise exceptions.InfluxDBConnectionError(err)
+
         except requests.exceptions.HTTPError as err:
+            if json_res and 'error' in json_res and\
+               json_res['error'].startswith('error parsing query'):
+                query = params['q']
+                raise exceptions.InfluxDBBadQueryError(query)
+
             if res.status_code == 400:
                 raise exceptions.InfluxDBBadRequestError(params)
             if res.status_code == 401:
