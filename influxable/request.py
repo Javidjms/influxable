8615a710807716a4e803fa3f6cc9f91d63b2d955
Javid Mougamadou <javidjms0@gmail.com>
2019-10-02 15:07:46 +0200

    [feat]: Add auth parameter in InfluxDbRequest constructor

--- influxable/request.py
+++ influxable/request.py
@@ -3,10 +3,11 @@ from urllib.parse import urljoin
 
 
 class InfluxDBRequest(requests.Session):
-    def __init__(self, base_url, database_name):
+    def __init__(self, base_url, database_name, auth):
         super().__init__()
         self.base_url = base_url
         self.database_name = database_name
+        self.auth = auth
 
     def request(self, method, url, **kwargs):
         return super().request(method, urljoin(self.base_url, url), **kwargs)
