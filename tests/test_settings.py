ce9992e6cb08e491b469a803a6248c103842c081
Javid Mougamadou <javidjms0@gmail.com>
2019-10-03 19:16:35 +0200

    [test]: Add test_check_url_exist() in TestSettings

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -5,3 +5,6 @@ class TestSettings:
     def check_if_variable_exist(self, variable_name):
         return hasattr(settings, variable_name)
 
+    def test_check_url_exist(self):
+        assert self.check_if_variable_exist('INFLUXDB_URL')
+
