525d524438a9cb700cab029de1b4c3519f5aaf1a
Javid Mougamadou <javidjms0@gmail.com>
2019-10-03 19:14:17 +0200

    [test]: Add test_create_instance_with_no_args_success() in TestConnection

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -8,3 +8,8 @@ class TestConnection:
         query = 'SHOW DATABASES'
         InfluxDBApi.execute_query(connection.request, query)
 
+    def test_create_instance_with_no_args_success(self):
+        connection = Connection()
+        self.check_if_connection_reached(connection)
+        assert connection is not None
+
