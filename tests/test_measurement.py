e058105c4321bb310728fbe32af0cb38c8c2673e
Javid Mougamadou <javidjms0@gmail.com>
2019-10-08 20:27:08 +0200

    [test]: Add TestMeasurement

---
+++ tests/test_measurement.py
@@ -0,0 +1,25 @@
+import pytest
+from decimal import Decimal as D
+from influxable import attributes, exceptions
+from influxable.db import Query
+from influxable.measurement import Measurement, MeasurementMeta
+from influxable.serializers import MeasurementPointSerializer
+
+
+class TestMeasurement:
+    def create_measurement_class(self):
+        class MySampleMeasurement(Measurement):
+            measurement_name = 'mysamplemeasurement'
+            time = attributes.TimestampFieldAttribute(precision="s")
+            value = attributes.IntegerFieldAttribute()
+        measurement_cls = MySampleMeasurement
+        return measurement_cls
+
+    def create_measurement_class_with_required(self):
+        class MySampleMeasurement(Measurement):
+            measurement_name = 'mysamplemeasurement'
+            time = attributes.TimestampFieldAttribute()
+            value = attributes.IntegerFieldAttribute(is_nullable=False)
+        measurement_cls = MySampleMeasurement
+        return measurement_cls
+
