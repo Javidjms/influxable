e8e6d14ee0814ca3530ed24fea616b1c336dbfaf
Javid Mougamadou <javidjms0@gmail.com>
2019-10-12 15:45:38 +0200

    [feat]: Add show_tag_values_cardinality() in InfluxDBAdmin

--- influxable/db/admin.py
+++ influxable/db/admin.py
@@ -54,6 +54,17 @@ class InfluxDBAdmin:
         return InfluxDBAdmin._execute_query(query, parser)
 
     @staticmethod
+    def show_tag_values_cardinality(key, exact=False):
+        options = {
+            'key': key,
+            'exact': 'EXACT' if exact else '',
+        }
+        query = 'SHOW TAG VALUES {exact} CARDINALITY WITH KEY = "{key}"'
+        query = query.format(**options)
+        parser = serializers.FormattedSerieSerializer
+        return InfluxDBAdmin._execute_query(query, parser)
+
+    @staticmethod
     def show_databases():
         query = 'SHOW DATABASES'
         parser = serializers.FlatSimpleResultSerializer
