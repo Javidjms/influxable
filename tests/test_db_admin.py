2ec3219134d0951d3de2135bb2aacc775f72b038
Javid Mougamadou <javidjms0@gmail.com>
2019-10-12 15:50:09 +0200

    [feat]: Add test_show_field_key_cardinality_with_exact_success() in TestDBAdminShowCommand

--- tests/test_db_admin.py
+++ tests/test_db_admin.py
@@ -6,3 +6,7 @@ class TestDBAdminShowCommand:
         res = InfluxDBAdmin.show_field_key_cardinality()
         assert res is not None
 
+    def test_show_field_key_cardinality_with_exact_success(self):
+        res = InfluxDBAdmin.show_field_key_cardinality(exact=True)
+        assert res is not None
+
