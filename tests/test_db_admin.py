2c3e989da46890d436522798c06c6687271ac6cc
Javid Mougamadou <javidjms0@gmail.com>
2019-10-12 15:53:27 +0200

    [feat]: Add test_show_tag_values_success() in TestDBAdminShowCommand

--- tests/test_db_admin.py
+++ tests/test_db_admin.py
@@ -120,3 +120,7 @@ class TestDBAdminShowCommand:
         res = InfluxDBAdmin.show_tag_keys('param1', 'param2')
         assert res is not None
 
+    def test_show_tag_values_success(self):
+        res = InfluxDBAdmin.show_tag_values('phase')
+        assert res is not None
+
