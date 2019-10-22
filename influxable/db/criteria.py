b1758396da48599074c44f516625a1ea951502ad
Javid Mougamadou <javidjms0@gmail.com>
2019-10-22 14:09:54 +0200

    [feat]: Rename WhereOperator into WhereOperatorEnum

--- influxable/db/criteria.py
+++ influxable/db/criteria.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 
-class WhereOperator(Enum):
+class WhereOperatorEnum(Enum):
     LT = 'lt'
     LTE = 'lte'
     GT = 'gt'
@@ -11,21 +11,21 @@ class WhereOperator(Enum):
 
 
 EVALUATED_OPERATORS = {
-    WhereOperator.LT: '<',
-    WhereOperator.LTE: '<=',
-    WhereOperator.GT: '>',
-    WhereOperator.GTE: '>=',
-    WhereOperator.EQ: '=',
-    WhereOperator.NE: '!=',
+    WhereOperatorEnum.LT: '<',
+    WhereOperatorEnum.LTE: '<=',
+    WhereOperatorEnum.GT: '>',
+    WhereOperatorEnum.GTE: '>=',
+    WhereOperatorEnum.EQ: '=',
+    WhereOperatorEnum.NE: '!=',
 }
 
 INVERTED_OPERATORS = {
-    WhereOperator.LT: WhereOperator.GTE,
-    WhereOperator.LTE: WhereOperator.GT,
-    WhereOperator.GT: WhereOperator.LTE,
-    WhereOperator.GTE: WhereOperator.LT,
-    WhereOperator.EQ: WhereOperator.NE,
-    WhereOperator.NE: WhereOperator.EQ,
+    WhereOperatorEnum.LT: WhereOperatorEnum.GTE,
+    WhereOperatorEnum.LTE: WhereOperatorEnum.GT,
+    WhereOperatorEnum.GT: WhereOperatorEnum.LTE,
+    WhereOperatorEnum.GTE: WhereOperatorEnum.LT,
+    WhereOperatorEnum.EQ: WhereOperatorEnum.NE,
+    WhereOperatorEnum.NE: WhereOperatorEnum.EQ,
 }
 
 
@@ -34,22 +34,22 @@ class Field:
         self.field_name = field_name
 
     def __lt__(self, value):
-        return Criteria(self, value, WhereOperator.LT)
+        return Criteria(self, value, WhereOperatorEnum.LT)
 
     def __le__(self, value):
-        return Criteria(self, value, WhereOperator.LTE)
+        return Criteria(self, value, WhereOperatorEnum.LTE)
 
     def __eq__(self, value):
-        return Criteria(self, value, WhereOperator.EQ)
+        return Criteria(self, value, WhereOperatorEnum.EQ)
 
     def __ne__(self, value):
-        return Criteria(self, value, WhereOperator.NE)
+        return Criteria(self, value, WhereOperatorEnum.NE)
 
     def __ge__(self, value):
-        return Criteria(self, value, WhereOperator.GTE)
+        return Criteria(self, value, WhereOperatorEnum.GTE)
 
     def __gt__(self, value):
-        return Criteria(self, value, WhereOperator.GT)
+        return Criteria(self, value, WhereOperatorEnum.GT)
 
     def __str__(self):
         return self.field_name
