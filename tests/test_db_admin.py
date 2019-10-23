bc74f3410049fa2755cfd74d4bf062bc95fe93fe
Javid Mougamadou <javidjms0@gmail.com>
2019-10-23 14:15:26 +0200

    [chore]: Rename TestDBAdminShowCommand into TestShowAdminCommand

--- tests/test_db_admin.py
+++ tests/test_db_admin.py
@@ -1,7 +1,7 @@
 from influxable.db import InfluxDBAdmin, Field
 
 
-class TestDBAdminShowCommand:
+class TestShowAdminCommand:
     def test_show_field_key_cardinality_success(self):
         res = InfluxDBAdmin.show_field_key_cardinality()
         assert res is not None
