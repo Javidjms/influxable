cafd150fef70eadf4a7c3ca0bed7ec03717f3229
Javid Mougamadou <javidjms0@gmail.com>
2019-11-17 14:30:55 +0100

    [feat]: Set reverse-loop for populate.py

--- populate.py
+++ populate.py
@@ -17,20 +17,22 @@ class TemperatureMeasurement(Measurement):
 
 
 def populate():
+    value_range = (15.4, 28.7)
     end_date = arrow.now('Europe/Paris').replace(hour=0, minute=0, second=0)
     start_date = end_date.shift(years=-NB_YEARS)
-    date_range = arrow.Arrow.span_range('month', start_date, end_date)
+    date_range = list(arrow.Arrow.span_range('month', start_date, end_date))
+    date_range.reverse()
     for current_start_month, current_end_month in date_range:
         points = []
-        phasis = ['moon', 'sun']
-        current_date = current_start_month
-        while current_date <= current_end_month:
+        phases = ['moon', 'sun']
+        current_date = current_end_month
+        while current_date >= current_start_month:
             points.append(TemperatureMeasurement(
-                phase=random.choice(phasis),
-                value=random.uniform(15.4, 28.7),
+                phase=random.choice(phases),
+                value=random.uniform(*value_range),
                 time=current_date.timestamp,
             ))
-            current_date = current_date.shift(minutes=1)
+            current_date = current_date.shift(minutes=-1)
         TemperatureMeasurement.bulk_save(points)
 
 
