096f57e406e28adee17878939e93c4487dc7c461
Javid Mougamadou <javidjms0@gmail.com>
2020-12-02 23:28:43 +0100

    [chore]: Set VERSION to 1.3.0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = '1.2.1'
+VERSION = '1.3.0'
 
 with open('requirements.txt', 'r') as f:
     requirements = [x.strip() for x in f if x.strip()]
